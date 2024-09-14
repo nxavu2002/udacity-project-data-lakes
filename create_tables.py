f18e97cd16c372c5d7c4a6173b864a8e3c2f92c1
AnhVu <113660820+nxavu2002@users.noreply.github.com>
2024-09-14 21:43:10 +0700

    Add files via upload

--- create_tables.py
+++ create_tables.py
@@ -4,12 +4,24 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """Drop any existing tables from database.
+
+         cur --    cursory to connected DB. Allows to execute SQL commands.
+         conn --   (psycopg2) connection to Postgres database (sparkifydb).
+         
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """Create new tables stagting_songs, staging_events, fact, dimension tables.
+
+         cur --    cursory to connected DB. Allows to execute SQL commands.
+         conn --   (psycopg2) connection to Postgres database (sparkifydb).
+         
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
--- etl.py
+++ etl.py
@@ -4,12 +4,28 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 into staging tables (staging_songs, staging_events) on Redshift using the queries in `copy_table_queries` list.
+        * cur --    reference to connected db.
+        * conn --   parameters (host, dbname, user, password, port)
+                    to connect the DB.
+
+    """
+
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging tables into fact and dimension tables on Redshift using the queries in `insert_table_queries` list.
+    
+        * cur --    reference to connected db.
+        * conn --   parameters (host, dbname, user, password, port)
+                    to connect the DB.
+
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
